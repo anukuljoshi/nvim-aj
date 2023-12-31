7a2924f0d3cdac47c0943430a3549f7643c354e8
anukuljoshi <anukul.joshi@geeksforgeeks.org>
2023-12-31 13:55:53 +0530

    plugin refactor, added hop

---
+++ after/plugin/hop.lua
@@ -0,0 +1,38 @@
+-- place this in one of your configuration file(s)
+local hop = require('hop')
+local directions = require('hop.hint').HintDirection
+
+function Search_char_bidirectional()
+    hop.hint_char1()
+end
+vim.keymap.set("", "s", Search_char_bidirectional)
+
+function Search_char2_bidirectional()
+    hop.hint_char2()
+end
+vim.keymap.set("", "<leader>s", Search_char2_bidirectional)
+
+function Search_words_forward()
+    hop.hint_words({
+        direction = directions.AFTER_CURSOR
+    })
+end
+vim.keymap.set("", "<leader>w", Search_words_forward)
+
+function Search_words_backward()
+    hop.hint_words({
+        direction = directions.BEFORE_CURSOR
+    })
+end
+vim.keymap.set("", "<leader>b", Search_words_backward)
+
+function Jump_lines()
+    hop.hint_vertical()
+end
+vim.keymap.set("", "<leader>g", Jump_lines)
+
+return {
+    'smoka7/hop.nvim',
+    version = "*",
+    opts = {}
+}
--- lua/plug/init.lua
+++ lua/plug/init.lua
@@ -10,73 +10,12 @@ vim.opt.rtp:prepend(lazypath)
 --     table: a list with your Plugin Spec
 --     string: a Lua module name that contains your Plugin Spec. See Structuring Your Plugins
 -- opts: see Configuration (optional)
-plugins = {require("plug.which-key"), require("plug.telescope"), require("plug.lsp"), require("plug.catpuccin")}
+plugins = {
+    require("plug.which-key"),
+    require("plug.telescope"),
+    require("plug.lsp"),
+    require("plug.catpuccin"),
+    require("plug.hop"),
+}
 
 require("lazy").setup(plugins, opts)
-
-local lsp_zero = require('lsp-zero')
-
-lsp_zero.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    lsp_zero.default_keymaps({
-        buffer = bufnr
-    })
-end)
-
--- see :help lsp-zero-guide:integrate-with-mason-nvim
--- to learn how to use mason.nvim with lsp-zero
-require('mason').setup({})
-require('mason-lspconfig').setup({
-    handlers = {lsp_zero.default_setup}
-})
-
-require("catppuccin").setup({
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
-        light = "latte",
-        dark = "mocha"
-    },
-    transparent_background = false, -- disables setting the background color.
-    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-    dim_inactive = {
-        enabled = false, -- dims the background color of inactive window
-        shade = "dark",
-        percentage = 0.15 -- percentage of the shade to apply to the inactive window
-    },
-    no_italic = false, -- Force no italic
-    no_bold = false, -- Force no bold
-    no_underline = false, -- Force no underline
-    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = {"italic"}, -- Change the style of comments
-        conditionals = {"italic"},
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {}
-    },
-    color_overrides = {},
-    custom_highlights = {},
-    integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = false,
-        mini = {
-            enabled = true,
-            indentscope_color = ""
-        }
-        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-    }
-})
-
--- setup must be called before loading
-vim.cmd.colorscheme "catppuccin"
